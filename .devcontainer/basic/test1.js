8e32e1bdae66366c24ddb13a29dd21b941f8e25d
Sangu-2301 <156165736+Sangu-2301@users.noreply.github.com>
2024-03-04 09:01:06 +0000

    conversion

--- .devcontainer/basic/test1.js
+++ .devcontainer/basic/test1.js
@@ -1,1 +1,14 @@
 console.log("sangeeta")
+let accountId ="rahul";
+const accountNumber =1687998;
+var accountCity ="pune";
+ myName="riya";
+ accountCity="jiya"
+ let accountState;
+ console.log(accountCity)
+ console.table([accountId,,accountNumber,accountCity,accountState])
+ /*
+ var is not prefer bcz issue in  block scope and function scope
+ */
+ console.log(typeof null)//object
+ console.log(typeof undefined)  
---
+++ .devcontainer/basic/test2.js
@@ -0,0 +1,16 @@
+// conversion
+let score = null;
+console.log(typeof score)
+let valueNumber=Number(score)
+console.log(valueNumber)
+// "33" =>33
+// 33abc =>NAN
+// true =>1;false =>0
+let isloggedIn =1
+let booleanIslogedIn=Boolean(isloggedIn)
+console.log(booleanIslogedIn)
+//  1 =>true; 0 =>false
+// "sangeeta"=>true
+let somenumber =33
+let stringNumber=String(somenumber)
+console.log(typeof stringNumber)
